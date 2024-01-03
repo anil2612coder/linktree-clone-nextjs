0bbed0802d22171d6694728a94d4fb9e14840986
Anil <ak26122k@gmail.com>
2024-01-03 22:39:22 +0530

    create user section

--- app/page.js
+++ app/page.js
@@ -7,8 +7,16 @@ export default function Home() {
   return (
    <main>
   <Navbar/>
-    <section>
-
+    <section className="p-4 pt-32 max-w-4xl mx-auto">
+      <div className="max-w-md mb-8">
+      <h1 className="text-6xl font-bold ">Your one link <br/>for everything</h1>
+     <h2 className="text-slate-500 text-xl mt-4 font-serif">Share your links, social profiles, contact info and more on one page</h2>
+      </div>
+      <form className=" items-center shadow-lg shadow-gray-400 inline-flex">
+        <span className="bg-white py-4 pl-4">linktree.to/</span>
+        <input className="py-4" type="text" placeholder="username"/>
+        <button type="submit" className="bg-blue-500 text-white py-4 px-6">Join For Free</button>
+      </form>
     </section>
    </main>
   )
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,10 +4,11 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <header className="bg-white p-3 border-b flex justify-between">
-      <div className="flex sm:gap-6 gap-2 ">
-      <Link className="font-bold text-xl text-center relative top-1 font-serif" href={"/"}><Image src={"/images/linktree-logo.png"} alt="logo" width={100} height={18}/></Link>
-       <nav className="flex gap-2 items-center text-slate-600">
+    <header className="bg-white py-4 border-b ">
+      <div className="max-w-4xl mx-auto flex justify-between ">
+      <div className="flex  sm:gap-6 gap-2 sm:px-7 px-3 ">
+      <Link className=" text-center   sm:h-7 sm:w-32 h-5 w-24 relative sm:top-0 top-1  font-serif" href={"/"}><Image src={"/images/logo-2.png"} alt="logo" fill={true}/></Link>
+       <nav className="flex gap-2 sm:gap-5 font-semibold items-center font-sans text-gray-400">
         <Link href={"/about"}>About</Link>
         <Link href={"/price"}>Price</Link>
         <Link href={"/contact"}>Contact</Link>
@@ -17,6 +18,8 @@ const Navbar = () => {
         <Link href={"/login"} className="bg-blue-600 pl-2 pr-2 border text-white pt-1 pb-1 rounded-md font-semibold font-sans overflow-hidden">Sign In</Link>
         <Link href={"/register"} className="bg-blue-600 pl-3 pr-3 border text-white pt-1 pb-1 rounded-md font-semibold font-sans overflow-hidden" >New Account</Link>
        </nav>
+      </div>
+     
     </header>
   )
 }
