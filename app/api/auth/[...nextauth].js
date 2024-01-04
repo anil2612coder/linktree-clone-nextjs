06de38205a9246f0cdaaae3a8f0239607248f77e
Anil <ak26122k@gmail.com>
2024-01-04 23:21:56 +0530

    sign in with google next-auth

---
+++ app/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import clientPromise from "@/libs/mongoClient";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      })
+  ],
+}
+
+export default NextAuth(authOptions)
